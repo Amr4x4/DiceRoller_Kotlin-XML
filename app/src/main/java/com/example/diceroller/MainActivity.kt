977d3909527e3938cbc74d6ef62e0647960dbbcd
Amr4x4 <amranwarx2@gmail.com>
2024-09-29 00:25:34 +0300

    Enhancing the XML

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,10 +3,8 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.delay
 import java.util.Random
 
 

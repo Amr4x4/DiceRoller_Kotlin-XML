cedf5ff4b9ccbc475c92cd49555f8fa02a5f6028
Amr4x4 <amranwarx2@gmail.com>
2024-09-28 23:41:17 +0300

    Adding Images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.diceroller
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.delay
+import java.util.Random
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        val rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton.setOnClickListener{
+            Toast.makeText(this, "Rolling....",Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.diceRollerImage)
+            val randomValue = Random().nextInt(6) + 1
+            val drawableRecourse = when (randomValue) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+            diceImage.setImageResource(drawableRecourse)
     }
 }
